2953fb08c32497bc38ba90103e5f4e1ce689b912
mounika <mounika.gg77@gmail.com>
2023-09-07 16:26:02 +0530

    CHANGES

--- route-53/main.tf
+++ route-53/main.tf
@@ -7,7 +7,7 @@ data "aws_route53_zone" "hosted_zone" {
 resource "aws_route53_record" "site_domain" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.record_name 
-  type    = "CNAME" 
+  type    = "A" 
 
   alias {
     name                   = var.application_load_balancer_dns_name
