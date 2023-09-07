cc2780ecb6ec8b387e60a03973464d37cf1e57dd
mounika <mounika.gg77@gmail.com>
2023-09-07 14:49:38 +0530

    created ecs module

--- ecs/variables.tf
+++ ecs/variables.tf
@@ -1,4 +1,4 @@
-/* # ecs variables
+# ecs variables
 variable "project_name" {}
 variable "environment" {}
 variable "ecs_task_execution_role_arn" {}
@@ -11,4 +11,3 @@ variable "private_app_subnet_az1_id" {}
 variable "private_app_subnet_az2_id" {}
 variable "app_server_security_group_id" {}
 variable "alb_target_group_arn" {}
- */
