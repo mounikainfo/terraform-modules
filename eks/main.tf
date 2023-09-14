abed9a01fdb6d160755bfc09f7df0814aca00853
mounika <mounika.gg77@gmail.com>
2023-09-14 14:27:56 +0530

    my changes

--- eks/main.tf
+++ eks/main.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 }
 
 resource "aws_eks_cluster" "demo" {
-  name     = var.cluster_name
+  name     = "demo"
   role_arn = aws_iam_role.demo.arn
 
   vpc_config {
