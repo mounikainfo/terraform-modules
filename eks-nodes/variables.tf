67903f13291f8bbe2800f4ce60a75d4196a78135
mounika <mounika.gg77@gmail.com>
2023-09-14 12:18:12 +0530

    changes

--- eks-nodes/variables.tf
+++ eks-nodes/variables.tf
@@ -1,7 +1,7 @@
 # nides variables
-variable "var.private_app_subnet_az1_id" {
+variable "private_app_subnet_az1_id" {
   
 }
-variable "var.private_app_subnet_az2_id" {
+variable "private_app_subnet_az2_id" {
   
 }
