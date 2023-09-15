aa8312c57383adb9928238d1dfa391cb12322b05
mounika <mounika.gg77@gmail.com>
2023-09-15 16:01:39 +0530

    new changes

--- nodegroup/main.tf
+++ nodegroup/main.tf
@@ -42,9 +42,9 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t2.small"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 5
-    min_size     = 0
+    min_size     = 1
   }
 
   update_config {
