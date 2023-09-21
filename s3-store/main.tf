efd6a85188d705b64b0c172755e7980c006ee518
mounika <mounika.gg77@gmail.com>
2023-09-22 00:58:44 +0530

    new changes

--- s3-store/main.tf
+++ s3-store/main.tf
@@ -2,7 +2,7 @@ resource "aws_flow_log" "flow-log-test" {
   log_destination      = aws_s3_bucket.my-log-test.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
-  vpc_id               = aws_vpc.STA-VPC.id
+  vpc_id               = aws_vpc.vpc.id
 }
 resource "aws_s3_bucket" "my-log-test" {
   bucket = "my-log-test"
