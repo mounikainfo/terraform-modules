258f83d9296fbb89fcd1e34b0bf711e92e11479b
mounika <mounika.gg77@gmail.com>
2023-09-23 12:32:37 +0530

    new changes

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -9,3 +9,7 @@ output "cluster_name" {
 output  "identity"  { 
   value = aws_eks_cluster.demo.identity[0].oidc[0].issuer
 }
+
+output "aws_iam_openid_connect_provider" {
+  value = aws_iam_openid_connect_provider.oidc_provider.arn
+}
